f75a4e4749afbef56424ccd4ed8d8283d6a68aaf
Nicholas Keane <NKeane@doetech.net>
2020-02-18 16:05:25 -0500

    chessboard layout updates

--- chess/src/components/ChessBoard.js
+++ chess/src/components/ChessBoard.js
@@ -8,17 +8,24 @@ export class ChessBoard extends React.Component {
 
         return (
             <div>
-                {sizeRange.map((y) => 
-                <div className="block-row"> {sizeRange.map((x) => {
-                    // Generate cross-hatch coloration grid for elements
-                    let shade;
-                    if (x % 2 === 0)
-                        shade = (y % 2) === 0 ? 'light' : 'dark';
-                    else
-                        shade = (y % 2) === 0 ? 'dark' : 'light';
-                    return <Block shadeType={shade}/>;
-                })}
-                </div> )}
+                <div style={{float: 'left'}}>
+                    {sizeRange.map((y) => 
+                    <div className="block-row"> {sizeRange.map((x) => {
+                        // Generate cross-hatch coloration grid for elements
+                        let shade;
+                        if (x % 2 === 0)
+                            shade = (y % 2) === 0 ? 'light' : 'dark';
+                        else
+                            shade = (y % 2) === 0 ? 'dark' : 'light';
+                        return <Block shadeType={shade}/>;
+                    })}
+                    </div> )}
+                </div>
+                <div style={{float: 'left'}}></div>
+                <div style={{textAlign: 'left'}}>Score: 0</div>
+                <div style={{textAlign: 'left'}}>Turn: 0</div>
+                <div style={{textAlign: 'left'}}>Last Move: (Piece)(0,0) to (Piece/Empty)(0,0)</div>
+                <div style={{clear: 'both'}} />
             </div>
         )
     }
