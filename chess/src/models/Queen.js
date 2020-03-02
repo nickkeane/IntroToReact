b2e37b4157308406442d37f3b9d761197b5ff908
Nicholas Keane <nickkeane23@gmail.com>
2020-03-02 10:52:05 -0500

    rename pos to off to make it a bit clearer

--- chess/src/models/Queen.js
+++ chess/src/models/Queen.js
@@ -12,13 +12,15 @@ export default class Queen extends Piece {
             [-1,  1], [0,  1], [1,  1],
         ]
 
-        return grid.map((pos, i) => { 
-            if (pos[0] === 0 && pos[1] === 0)
-                return [[src[0] + pos[0], src[1] + pos[1]]];
+        return grid.map((off, i) => {
+            // center 
+            if (off[0] === 0 && off[1] === 0)
+                return [[src[0] + off[0], src[1] + off[1]]];
             
+            // sides and angles
             let spots = [];
             for (let j = 0; j < 8; ++j)
-                spots.push([pos[0] * j, pos[1] * j]);
+                spots.push([off[0] * j, off[1] * j]);
             return spots.map((obj, i) => [src[0] + obj[0], src[1] + obj[1]]);
         }).filter((pos, i) => dest[0] === pos[0] && dest[1] === pos[1])
         .length >= 1;
