e1e0be2a859872cebf34df08fa717b73714e0936
Nicholas Keane <nickkeane23@gmail.com>
2020-03-02 14:29:21 -0500

    add Bishop and Rook code

--- chess/src/models/ChessBoard.js
+++ chess/src/models/ChessBoard.js
@@ -6,4 +6,16 @@ export class ChessBoard {
         for (let i = 0; i < 8; ++i)
             this._board[i] = new Array(8).fill(null);
     }
+
+    get board() {
+        return this._board;
+    }
+
+//     get boardRow(row) {
+//         return this.board[row];
+//     }
+
+//     get boardRowCol(row, col) {
+//         return this.board[row][col];
+//     }
 }
---
+++ chess/src/models/Rook.js
@@ -0,0 +1,42 @@
+import Piece, { pieceColor, pieceType } from './Piece'
+
+export default class Bishop extends Piece {
+    constructor(color) {
+        super(color, PieceType.Bishop);
+    }
+
+    isMovePossible(src, dest) {
+        const grid = [
+                      [0, -1],
+            [-1,  0], [0,  0], [1,  0],
+                      [0,  1]
+        ]
+        const extrusion = 8; // How far to extrude directions outwards?
+
+        return grid.map((off, i) => {
+            // center 
+            if (off[0] === 0 && off[1] === 0)
+                return [[src[0] + off[0], src[1] + off[1]]];
+            
+            // sides and angles
+            let spots = [];
+            for (let j = 0; j < extrusion; ++j)
+                spots.push([off[0] * j, off[1] * j]);
+            return spots.map((obj, i) => [src[0] + obj[0], src[1] + obj[1]]);
+        }).filter((pos, i) => {
+            let match = false;
+            for (let pos2 of pos) { 
+                if (dest[0] === pos2[0] && dest[1] === pos2[1]) {
+                    match = true;
+                    break;
+                }
+            }
+            return match;
+        })
+        .length >= 1;
+    }
+
+    getPath(src, dest) {
+        return [];
+    }
+}
