6e0ac84dfa985c8d18549c6cba6484a4f712d0f8
Nicholas Keane <nickkeane23@gmail.com>
2020-03-11 16:42:49 -0400

    add FEN piecePlacement function

--- chess/src/models/ChessBoard.js
+++ chess/src/models/ChessBoard.js
@@ -1,3 +1,55 @@
+import { pieceColor, pieceType } from "./Piece";
+
+function _fenPiecePlacement(board) {
+    let out = '';
+    let spaces = 0;
+    for (let x = 0; x < board.length; ++x) {
+        for (let y = 0; y < board.length; ++y) {
+            let piece = board[x][y];
+            if (piece === null) {
+                ++spaces;
+                continue;
+            }
+
+            if (spaces) {
+                out += spaces.toString();
+                spaces = 0;
+            }
+
+            let letter = '';
+            switch (piece.type) {
+                case pieceType.rook:
+                    letter = 'R';
+                    break;
+                case pieceType.knight:
+                    letter = 'N';
+                    break;
+                case pieceType.bishop:
+                    letter = 'B';
+                    break;
+                case pieceType.queen:
+                    letter = 'Q';
+                    break;
+                case pieceType.king:
+                    letter = 'K';
+                    break;
+                case pieceType.pawn:
+                    letter = 'P';
+                    break;
+            }
+            letter = (piece.color === pieceColor.black) ? letter.toLowerCase() : letter;
+            out += letter;
+        }
+
+        if (spaces) {
+            out += spaces.toString();
+            spaces = 0;
+        }
+
+        out += '/';
+    }
+}
+
 export default class ChessBoard {
     _board = null;
 
@@ -9,6 +61,14 @@ export default class ChessBoard {
 
     get board() {
         return this._board;
+    }
+
+    get toFen() {
+        let out = [];
+        out.push(
+            _fenPiecePlacement(this._board)
+        );
+        return out.join('');
     }
 
 //     get boardRow(row) {
